8eeaa7fd9ebec91d3cbba3d9d10053ab56a248ac
Bryn Edwards <brynedwards@mailbox.org>
2022-07-06 11:39:08 +0100

    Add shellHook

--- template/flake.nix
+++ template/flake.nix
@@ -4,6 +4,9 @@
   outputs = { self, nixpkgs }: {
     devShell.x86_64-linux =
       with import "${nixpkgs}" { system = "x86_64-linux"; };
-      mkShell { buildInputs = [ ]; };
+      mkShell {
+        buildInputs = [ ];
+        shellHook = "";
+      };
   };
 }
